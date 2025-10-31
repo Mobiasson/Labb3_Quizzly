b018fc041d5ea0ef9ee04df48ef20f6dcff7b472
Mobiasson <117571587+MRosTobiasson@users.noreply.github.com>
2025-10-31 16:24:12 +0100

    Added code-alongs and started MainWindow.cs

---
+++ Quizzly/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Quizzly
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Quizzly/ViewModels/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Quizzly.ViewModels;
+
+internal class ViewModelBase : INotifyPropertyChanged {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void RaisePropertyChanged([CallerMemberName] string? propertyName = null) {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
