5368cbb05ba9c9abe031f77705617594b986bf0b
Mobiasson <mikaeltobiasson@hotmail.com>
2025-11-12 11:55:56 +0100

    Fixed EndView

--- Quizzly/ViewModels/EndViewModel.cs
+++ Quizzly/ViewModels/EndViewModel.cs
@@ -1,6 +1,4 @@
-﻿using Quizzly.ViewModels;
-
-namespace Quizzly.ViewModels;
+﻿namespace Quizzly.ViewModels;
 public class EndViewModel : ViewModelBase {
     private readonly MainWindowViewModel _mainVM;
 
@@ -12,5 +10,14 @@ public class EndViewModel : ViewModelBase {
     public int CorrectAnswers { get; }
     public int TotalQuestions { get; }
 
-    public string ResultText => $"{CorrectAnswers} out of {TotalQuestions} questions were correct";
+    public string EndText => $"Game is over. You did so well wow......";
+
+    public string ResultText =>
+    (TotalQuestions - CorrectAnswers) switch {
+        0 => $"Excellent! {CorrectAnswers}/{TotalQuestions} Maybe you're not useless after all",
+        1 or 2 => $"Not so bad, not so good either.. {CorrectAnswers}/{TotalQuestions}",
+        3 or 4 or 5 => $"Did you skip preschool? {CorrectAnswers}/{TotalQuestions}",
+        _ => $"HAHAHAH {CorrectAnswers}/{TotalQuestions}"
+    };
 }
+
